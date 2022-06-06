7a00bbccd7ef861a4ff9c0517e11aa260ac6131b
changwon <hamin3822@gmail.com>
2022-06-07 00:57:31 +0900

    수정
    + 속도 개선
    + 오류 수정
    + 진행 상태 표시 추가

--- scan_port.py
+++ scan_port.py
@@ -1,9 +1,10 @@
 import socket
 import re, sys
 import threading
+from tqdm import tqdm
 
 print_lock = threading.Semaphore(value=1) 
-maxConnection = 100          
+maxConnection = 10000          
 connection_lock = threading.BoundedSemaphore(value=maxConnection) 
 port_regex = re.compile("([0-9]+){1,5}-([0-9]+){1,5}")
 ip_regex = re.compile("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
@@ -13,9 +14,6 @@ access_port = []
 def scan_port(address, port_num) :
     global access_port
     try:
-        print_lock.acquire()
-        print(f'{port_num}', end='\r')
-        print_lock.release()
         response_data = None
         scan = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         scan.connect((address, port_num))
@@ -23,6 +21,8 @@ def scan_port(address, port_num) :
         if response_data:
             access_port.append(port_num)
         scan.close()
+    except KeyboardInterrupt:
+        sys.exit()
     except:
         pass
     finally:
@@ -34,20 +34,23 @@ def scan_port(address, port_num) :
     
 def scan_info():
     while True :
-        input_address = input("IP 또는 URL 입력 : ")
-        if url_regex.search(input_address.replace(" ","")) is not None and (ip_regex.search(input_address.replace(" ","")) is not None or ip_regex.search(input_address.replace(" ","")) is None):
-            domain_address = re.sub("http(s)?:\/\/", '', input_address)
-            result_address = domain_address.strip('/')
-            ip_address = socket.gethostbyname(result_address)
-            for num in range(1,65536): # 포트 범위 65535까지 검색
-                connection_lock.acquire()
-                t = threading.Thread(target=scan_port, args=(ip_address, num))
-                t.start()
+        try : 
+            input_address = input("IP 또는 URL 입력 : ")
+            if url_regex.search(input_address.replace(" ","")) is not None and (ip_regex.search(input_address.replace(" ","")) is not None or ip_regex.search(input_address.replace(" ","")) is None):
+                domain_address = re.sub("http(s)?:\/\/", '', input_address)
+                result_address = domain_address.strip('/')
+                ip_address = socket.gethostbyname(result_address)
+                for num in tqdm(range(1,65536)): # 포트 범위 65535까지 검색
+                    connection_lock.acquire()
+                    t = threading.Thread(target=scan_port, args=(ip_address, num))
+                    t.start()
             break
-        else :
+        except KeyboardInterrupt:
+            sys.exit()
+        except :
             print("잘못된 입력입니다. 다시 입력해주세요.\n")
             continue
 
-# if __name__ == "__main__":
-#     scan_info()
-#     print("\n",access_port)
+if __name__ == "__main__":
+    scan_info()
+    print("\n",access_port)
